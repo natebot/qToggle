90b4d2e54fdf57e6ad652c46ef53e960052d0250
natebot <natebot@gmail.com>
2015-12-03 00:42:57 -0800

    trim whitespace

--- jquery.qtoggle.js
+++ jquery.qtoggle.js
@@ -31,24 +31,23 @@
 		// settings with defaults
 		var settings = $.extend({
 			'data'        : {},                         // object - passed into event (reserved for later use)
-			'effect'      : 'toggle',              // string - the animation effect on target
-			'events'      : 'click.qToggle',      		// string - the event on control (namespaced)
-			'context'     : '[data-qtoggle-selector]',  // string - a selector that defines a control
-			'selector'    : 'qtoggle-selector',         // string - the name of the data that holds selector string of target
+			'effect'      : 'toggle', 					// string - the animation effect on target
+			'events'      : 'click.qToggle',			// string - the event on control (namespaced)
+			'context'     : '[data-qtoggle-selector]',	// string - a selector that defines a control
+			'selector'    : 'qtoggle-selector',			// string - the name of the data that holds selector string of target
 			'targets'     : 'prev',                     // string - the default target if none is specified in settings.selector
 			'innerHTML'   : '',                         // string - html to insert into control's text node. Empty string for no change
 			'toggleClass' : '',							// string - a class name to toggle if using the toggleClass effect
 			'eventArgs'   : {							// object - arguments passed to the effect function
 				'duration' 	: 'fast',					// int|string - duration of animation in miliseconds or keywords 'fast','slow',etc. Note that the default 'toggle' effect will always have 0 duration
-				'easing' 	: 'linear',					// string - the animation effect, 'linear' or 'swing' are the only options in native jQuery but other plugins may provide other options
+				'easing' 	:'linear',					// string - the animation effect, 'linear' or 'swing' are the only options in native jQuery but other plugins may provide other options
 				'callback' 	: null						// string|function - the function to call when animation is complete
-				}																			
+				}
 		}, options);
-	
 
 	// listen for events on the selector
 	this.on( settings.events, settings.context, settings.data, function(event){
-		
+
 		// explicitly stop default behavior of events on this control
 		event.preventDefault();
 		// explicitly stop bubbling
@@ -56,44 +55,44 @@
 
 		/* @var object - a whitelist of available effects and DOM transversal keywords */
 		var defaults = {
-			'effects' :[ 'toggle', 'slideToggle', 'fadeToggle', 'hide' , 'show' , 'fadeOut', 'fadeIn', 'slideUp' , 'slideDown', 'toggleClass' ],
+			'effects' :[ 'toggle', 'slideToggle', 'fadeToggle', 'hide' , 'show' , 'fadeOut', 'fadeIn', 'slideUp', 'slideDown', 'toggleClass' ],
 			'transversals' : [ 'prev' ,'next', 'parent', 'siblings', 'nextAll', 'prevAll' ,'children' ]
 		};
 
 		/* @var object - the jQuery selection of the toggle control element */
 		var control = jQuery(this);
-		
+
 		// @todo throw an error if given effect is not in our whitelist
 		/* @var string - the name of the effect function desired, 'toggle' by default */
 		var effect  = control.data('qtoggle-effect') || settings.effect;
 		effect      = (jQuery.inArray( effect, defaults.effects ) === -1) ? defaults.effects[0] : effect;
-		
+
 		/* @var string - the selector string for our targeted DOM element to be effected */
 		var targets 	=  control.data( settings.selector ) || settings.targets;
 
-		/* @var string - the new value of the control's innerHTML, if any */ 
+		/* @var string - the new value of the control's innerHTML, if any */
 		var innerHTML 	=  ( control.data('qtoggle-text') || settings.innerHTML );
 
-		/* @var string|int - the timing for the animation */ 
+		/* @var string|int - the timing for the animation */
 		var duration 	= control.data('qtoggle-duration') || settings.eventArgs.duration;
 		/* 'toggle' effect should *always* be instantanious regardless of passed duration setting */
 		if( effect === 'toggle' ){
 			duration = 0;
 		}
 
-		/* @var string - the timing for the animation */ 
+		/* @var string - the timing for the animation */
 		var easing    	= control.data('qtoggle-easing') || settings.eventArgs.easing;
 
 		/* @var function|null - the callback function, if any */
-		var callback 	=  control.data('qtoggle-callback') || settings.eventArgs.callback;			
+		var callback 	=  control.data('qtoggle-callback') || settings.eventArgs.callback;
 		callback 		= (typeof window[callback] === 'function' ) ? window[callback] : null
 
 		// possibly update the innerHTML of the control element
 		if( innerHTML ) {
 			 control.data( 'qtoggle-text', control.html() );
-			 control.html( innerHTML ); 
+			 control.html( innerHTML );
 		}
-		
+
 		// exceptions to standard effect arguments:
 		// if we are toggling a class then the first argument passed to effect function is a class name not a duration
 		if( effect === 'toggleClass' ){
@@ -118,6 +117,6 @@
 	});// end this.on()
 
 	// I'm a chainable function
-	return this; 
+	return this;
 
 }})( jQuery );
